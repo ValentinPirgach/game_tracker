eae8b79b9617979bb7d5f6f071b349e6a7568d88
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-07-20 01:01:15 +0300

    Procces adding game

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,8 +7,16 @@ GameTracker.config(['$httpProvider', function ($httpProvider) {
 
 GameTracker.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.games = [];
+  $scope.game = {name: '', store: ''};
 
   $http.get('/games').then(function (resp) {
     $scope.games = resp.data.games;
   })
+
+  $scope.createGame = function (form) {
+    $http.post('/games', $scope.game).then(function (resp) {
+      $scope.games.push(resp.data.game);
+      $scope.game = {name: '', store: ''};
+    })
+  };
 }]);
